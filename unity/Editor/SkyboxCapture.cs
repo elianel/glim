c413b1f567e7959474e4ab73d68d06df7db32426
z3y <z3y@protonmail.com>
2026-07-18 20:45:13 +0200

    fix skybox color space

--- unity/Editor/SkyboxCapture.cs
+++ unity/Editor/SkyboxCapture.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Experimental.Rendering;
+using UnityEngine.SceneManagement;
 
 namespace Glim
 {
     public static class SkyboxCapture
     {
-        public const int RESOLUTION = 128;
+        public const int RESOLUTION = 256;
+        const GraphicsFormat FORMAT = GraphicsFormat.R16G16B16A16_SFloat;
 
         public static Color[] Capture(Scene scene)
         {
-            var cubemap = new Cubemap(
-                RESOLUTION,
-                TextureFormat.RGBAHalf,
-                false,
-                true
-            );
+            var rtDesc = new RenderTextureDescriptor(RESOLUTION, RESOLUTION)
+            {
+                dimension = UnityEngine.Rendering.TextureDimension.Cube,
+                graphicsFormat = FORMAT,
+                depthBufferBits = 24,
+                msaaSamples = 1
+            };
+
+            var rt = new RenderTexture(rtDesc);
+            rt.Create();
 
             var cameraGO = new GameObject("Skybox Capture Camera");
             SceneManager.MoveGameObjectToScene(cameraGO, scene);
@@ -27,18 +32,28 @@ namespace Glim
             camera.backgroundColor = Color.black;
             camera.allowHDR = true;
 
-            camera.RenderToCubemap(cubemap);
+            camera.RenderToCubemap(rt);
 
             Color[] pixels = new Color[RESOLUTION * RESOLUTION * 6];
+            var face = new Texture2D(RESOLUTION, RESOLUTION, FORMAT, TextureCreationFlags.None);
 
             for (int faceIndex = 0; faceIndex < 6; faceIndex++)
             {
-                var colors = cubemap.GetPixels((CubemapFace)faceIndex);
+                Graphics.SetRenderTarget(rt, 0, (CubemapFace)faceIndex);
+
+                face.ReadPixels(new Rect(0, 0, RESOLUTION, RESOLUTION), 0, 0);
+                face.Apply(false);
+
+                var colors = face.GetPixels();
                 colors.CopyTo(pixels, faceIndex * RESOLUTION * RESOLUTION);
             }
 
+            Graphics.SetRenderTarget(null);
+
             Object.DestroyImmediate(cameraGO);
-            Object.DestroyImmediate(cubemap);
+            Object.DestroyImmediate(face);
+            rt.Release();
+            Object.DestroyImmediate(rt);
 
             return pixels;
         }
